2b5a46513f5a33550fa5ad0a25f61ef282f95d2a
ETdoFresh <ETdoFresh@gmail.com>
2020-02-29 20:03:35 -0600

    Crank up gravity * 10. Add Movement which includes left and right velocity movement and impulse force jump.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour
+{
+    public float movementVelocity = 10;
+    public float jumpForce = 30;
+    public new Rigidbody2D rigidbody2D;
+    private bool jump;
+
+    private void OnValidate()
+    {
+        if (!rigidbody2D) rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+            jump = true;
+    }
+
+    private void FixedUpdate()
+    {
+        var velocityY = new Vector2(0, rigidbody2D.velocity.y);
+        var input = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+        rigidbody2D.velocity = input * movementVelocity + velocityY;
+
+        if (jump)
+        {
+            input = new Vector2(0, 1);
+            rigidbody2D.AddForce(input * jumpForce, ForceMode2D.Impulse);
+            jump = false;
+        }
+    }
+}
